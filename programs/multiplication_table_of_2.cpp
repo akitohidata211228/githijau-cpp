827ded7d7333157eabb782aac8d3af6aa3bff9ec
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 20:20:24 +0700

    feat: add Tabel Perkalian 2 (C++)

---
+++ programs/multiplication_table_of_2.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_2.cpp
+// Tabel perkalian 2.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "2 x " << i << " = " << (2 * i) << "\n";
+    }
+    return 0;
+}
