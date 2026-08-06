6d889ded3a5bd569b0e54836baf82ef74ebb802d
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 22:24:33 +0700

    feat: add Tabel Perkalian 3 (C++)

---
+++ programs/multiplication_table_of_3.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_3.cpp
+// Tabel perkalian 3.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "3 x " << i << " = " << (3 * i) << "\n";
+    }
+    return 0;
+}
