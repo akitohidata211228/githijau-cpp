3450e3d7432bb046822ec21e6ad402aa89a9904f
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 12:33:21 +0700

    feat: add Hello World (C++)

---
+++ programs/hello_world.cpp
@@ -0,0 +1,10 @@
+// hello_world.cpp
+// Program pertama dalam C++.
+// Kompilasi: g++ hello_world.cpp -o hello_world && ./hello_world
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
