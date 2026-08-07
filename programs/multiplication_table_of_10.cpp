b505d91bdf96d057f3e81c7c8c363286df7e7ad0
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 19:42:11 +0700

    feat: add Tabel Perkalian 10 (C++)

---
+++ programs/multiplication_table_of_10.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_10.cpp
+// Tabel perkalian 10.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "10 x " << i << " = " << (10 * i) << "\n";
+    }
+    return 0;
+}
