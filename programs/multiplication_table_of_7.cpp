4efa99c47498b23c3dcd68cf7d65bb7ae5e0b93f
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 14:33:42 +0700

    feat: add Tabel Perkalian 7 (C++)

---
+++ programs/multiplication_table_of_7.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_7.cpp
+// Tabel perkalian 7.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "7 x " << i << " = " << (7 * i) << "\n";
+    }
+    return 0;
+}
