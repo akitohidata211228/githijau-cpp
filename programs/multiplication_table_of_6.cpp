d0c45366d33afa6a84dabeed9034e4a44de3b013
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 12:41:43 +0700

    feat: add Tabel Perkalian 6 (C++)

---
+++ programs/multiplication_table_of_6.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_6.cpp
+// Tabel perkalian 6.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "6 x " << i << " = " << (6 * i) << "\n";
+    }
+    return 0;
+}
