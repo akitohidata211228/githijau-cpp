2ac7244301984b83ac32299c5bfe9ff1ebc4fb58
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 09:20:33 +0700

    feat: add Tabel Perkalian 4 (C++)

---
+++ programs/multiplication_table_of_4.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_4.cpp
+// Tabel perkalian 4.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "4 x " << i << " = " << (4 * i) << "\n";
+    }
+    return 0;
+}
