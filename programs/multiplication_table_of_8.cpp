527a62ec476d86550260d821411821a3335a713d
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 16:07:42 +0700

    feat: add Tabel Perkalian 8 (C++)

---
+++ programs/multiplication_table_of_8.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_8.cpp
+// Tabel perkalian 8.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "8 x " << i << " = " << (8 * i) << "\n";
+    }
+    return 0;
+}
