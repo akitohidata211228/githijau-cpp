11b30974356c58355b4c00da02d4ab4339ab96be
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 10:56:42 +0700

    feat: add Tabel Perkalian 5 (C++)

---
+++ programs/multiplication_table_of_5.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_5.cpp
+// Tabel perkalian 5.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "5 x " << i << " = " << (5 * i) << "\n";
+    }
+    return 0;
+}
