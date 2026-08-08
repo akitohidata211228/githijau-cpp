6c01077cfc8e4a60313701a20f3e1cd3c6382ebc
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 20:56:21 +0700

    feat: add Tabel Perkalian 18 (C++)

---
+++ programs/multiplication_table_of_18.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_18.cpp
+// Tabel perkalian 18.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "18 x " << i << " = " << (18 * i) << "\n";
+    }
+    return 0;
+}
