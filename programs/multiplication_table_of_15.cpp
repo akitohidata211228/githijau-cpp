d02a6bb67e30516792233c4a9e5f48833c2ed7e6
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 20:16:58 +0700

    feat: add Tabel Perkalian 15 (C++)

---
+++ programs/multiplication_table_of_15.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_15.cpp
+// Tabel perkalian 15.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "15 x " << i << " = " << (15 * i) << "\n";
+    }
+    return 0;
+}
