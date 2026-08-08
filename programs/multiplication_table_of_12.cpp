8ca995bc1278a49f53aff4524edf343ba929a490
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 09:07:12 +0700

    feat: add Tabel Perkalian 12 (C++)

---
+++ programs/multiplication_table_of_12.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_12.cpp
+// Tabel perkalian 12.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "12 x " << i << " = " << (12 * i) << "\n";
+    }
+    return 0;
+}
