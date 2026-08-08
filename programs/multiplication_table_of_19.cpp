518690947216a76cde62b99df9b6a87cb7ec3866
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 21:21:03 +0700

    feat: add Tabel Perkalian 19 (C++)

---
+++ programs/multiplication_table_of_19.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_19.cpp
+// Tabel perkalian 19.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "19 x " << i << " = " << (19 * i) << "\n";
+    }
+    return 0;
+}
