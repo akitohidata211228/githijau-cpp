329dc2aa09205ec3f662628d131acd81ff9ad224
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 20:32:21 +0700

    feat: add Tabel Perkalian 16 (C++)

---
+++ programs/multiplication_table_of_16.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_16.cpp
+// Tabel perkalian 16.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "16 x " << i << " = " << (16 * i) << "\n";
+    }
+    return 0;
+}
