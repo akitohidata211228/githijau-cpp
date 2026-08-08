2b9f5c777c24f485150cf33cab6b1e61c29250fa
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 13:35:26 +0700

    feat: add Tabel Perkalian 13 (C++)

---
+++ programs/multiplication_table_of_13.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_13.cpp
+// Tabel perkalian 13.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "13 x " << i << " = " << (13 * i) << "\n";
+    }
+    return 0;
+}
