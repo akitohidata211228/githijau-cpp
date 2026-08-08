0cdab691e32a1800236abb64ca9eb837b2b4bcd9
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 20:44:22 +0700

    feat: add Tabel Perkalian 17 (C++)

---
+++ programs/multiplication_table_of_17.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_17.cpp
+// Tabel perkalian 17.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "17 x " << i << " = " << (17 * i) << "\n";
+    }
+    return 0;
+}
