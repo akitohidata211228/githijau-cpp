93e3a77208a5b0d2e8c310628ef27bc9ec7e04c3
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 19:43:06 +0700

    feat: add Tabel Perkalian 26 (C++)

---
+++ programs/multiplication_table_of_26.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_26.cpp
+// Tabel perkalian 26.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "26 x " << i << " = " << (26 * i) << "\n";
+    }
+    return 0;
+}
