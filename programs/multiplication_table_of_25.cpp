2c000290cd6c31d5735ed93851c4cfcaa301cf21
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 17:43:45 +0700

    feat: add Tabel Perkalian 25 (C++)

---
+++ programs/multiplication_table_of_25.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_25.cpp
+// Tabel perkalian 25.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "25 x " << i << " = " << (25 * i) << "\n";
+    }
+    return 0;
+}
