5dc3d37ae78b6cba1a3cb7f8255d8572e1e821b4
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 13:56:03 +0700

    feat: add Tabel Perkalian 22 (C++)

---
+++ programs/multiplication_table_of_22.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_22.cpp
+// Tabel perkalian 22.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "22 x " << i << " = " << (22 * i) << "\n";
+    }
+    return 0;
+}
