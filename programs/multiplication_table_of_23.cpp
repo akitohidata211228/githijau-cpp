855d32c603bfe27ad77f884d5641cc577028cd51
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 14:09:04 +0700

    feat: add Tabel Perkalian 23 (C++)

---
+++ programs/multiplication_table_of_23.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_23.cpp
+// Tabel perkalian 23.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "23 x " << i << " = " << (23 * i) << "\n";
+    }
+    return 0;
+}
