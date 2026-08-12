e8cf610d275a88fdfe158876c74d3e786d68817b
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 13:30:03 +0700

    feat: add Tabel Perkalian 20 (C++)

---
+++ programs/multiplication_table_of_20.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_20.cpp
+// Tabel perkalian 20.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "20 x " << i << " = " << (20 * i) << "\n";
+    }
+    return 0;
+}
