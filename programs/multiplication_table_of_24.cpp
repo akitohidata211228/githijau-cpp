08ebcee449cb546e4e1cd1be1f0e845cbe41ad42
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 16:11:26 +0700

    feat: add Tabel Perkalian 24 (C++)

---
+++ programs/multiplication_table_of_24.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_24.cpp
+// Tabel perkalian 24.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "24 x " << i << " = " << (24 * i) << "\n";
+    }
+    return 0;
+}
