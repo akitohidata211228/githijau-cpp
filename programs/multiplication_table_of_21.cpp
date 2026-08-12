c4880ef5f16e8cb7a999e92fa0fd5f78cd717c4d
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 13:43:03 +0700

    feat: add Tabel Perkalian 21 (C++)

---
+++ programs/multiplication_table_of_21.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_21.cpp
+// Tabel perkalian 21.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "21 x " << i << " = " << (21 * i) << "\n";
+    }
+    return 0;
+}
