20bf8846422cb550973c24a02db360ce655e29df
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-13 18:09:08 +0700

    feat: add Tabel Perkalian 31 (C++)

---
+++ programs/multiplication_table_of_31.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_31.cpp
+// Tabel perkalian 31.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "31 x " << i << " = " << (31 * i) << "\n";
+    }
+    return 0;
+}
