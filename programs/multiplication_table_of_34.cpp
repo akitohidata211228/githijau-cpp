3aa326924b33c83cf43c0ced9be945fcf2bb5457
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-13 22:40:56 +0700

    feat: add Tabel Perkalian 34 (C++)

---
+++ programs/multiplication_table_of_34.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_34.cpp
+// Tabel perkalian 34.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "34 x " << i << " = " << (34 * i) << "\n";
+    }
+    return 0;
+}
