f634dd9b269a713519ea08a587c376b37f3052a4
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-13 18:45:56 +0700

    feat: add Tabel Perkalian 32 (C++)

---
+++ programs/multiplication_table_of_32.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_32.cpp
+// Tabel perkalian 32.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "32 x " << i << " = " << (32 * i) << "\n";
+    }
+    return 0;
+}
