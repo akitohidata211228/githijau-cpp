9c85355aa42f8f0317f10cd55ee285d460e9fd64
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-13 13:50:25 +0700

    feat: add Tabel Perkalian 28 (C++)

---
+++ programs/multiplication_table_of_28.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_28.cpp
+// Tabel perkalian 28.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "28 x " << i << " = " << (28 * i) << "\n";
+    }
+    return 0;
+}
