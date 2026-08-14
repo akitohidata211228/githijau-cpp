ae653cb141145440ad79f7638f7d79e44875ec50
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-14 14:03:10 +0700

    feat: add Tabel Perkalian 38 (C++)

---
+++ programs/multiplication_table_of_38.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_38.cpp
+// Tabel perkalian 38.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "38 x " << i << " = " << (38 * i) << "\n";
+    }
+    return 0;
+}
