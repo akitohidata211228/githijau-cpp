26257b383ae8b582757ed7112097427c5882df32
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-14 18:55:09 +0700

    feat: add Tabel Perkalian 40 (C++)

---
+++ programs/multiplication_table_of_40.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_40.cpp
+// Tabel perkalian 40.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "40 x " << i << " = " << (40 * i) << "\n";
+    }
+    return 0;
+}
