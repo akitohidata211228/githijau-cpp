7fbbe31230462ac1522deb386f417e7aec51875b
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-14 11:22:03 +0700

    feat: add Tabel Perkalian 36 (C++)

---
+++ programs/multiplication_table_of_36.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_36.cpp
+// Tabel perkalian 36.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "36 x " << i << " = " << (36 * i) << "\n";
+    }
+    return 0;
+}
