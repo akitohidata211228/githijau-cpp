4be4075c10034e6b94fb20337168cab7e4c2bf1c
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-14 09:28:56 +0700

    feat: add Tabel Perkalian 35 (C++)

---
+++ programs/multiplication_table_of_35.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_35.cpp
+// Tabel perkalian 35.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "35 x " << i << " = " << (35 * i) << "\n";
+    }
+    return 0;
+}
