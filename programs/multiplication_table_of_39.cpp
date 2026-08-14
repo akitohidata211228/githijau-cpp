a51baf6efc3a0f50e64dbe823baefe781c308f6b
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-14 16:23:09 +0700

    feat: add Tabel Perkalian 39 (C++)

---
+++ programs/multiplication_table_of_39.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_39.cpp
+// Tabel perkalian 39.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "39 x " << i << " = " << (39 * i) << "\n";
+    }
+    return 0;
+}
