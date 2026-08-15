3a8241a167a00c955d647d7f05414197547d61fe
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-15 16:07:25 +0700

    feat: add Tabel Perkalian 46 (C++)

---
+++ programs/multiplication_table_of_46.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_46.cpp
+// Tabel perkalian 46.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "46 x " << i << " = " << (46 * i) << "\n";
+    }
+    return 0;
+}
