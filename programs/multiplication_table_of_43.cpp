55fef2236c9534d16753973a380cfe679215d797
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-15 09:36:35 +0700

    feat: add Tabel Perkalian 43 (C++)

---
+++ programs/multiplication_table_of_43.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_43.cpp
+// Tabel perkalian 43.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "43 x " << i << " = " << (43 * i) << "\n";
+    }
+    return 0;
+}
