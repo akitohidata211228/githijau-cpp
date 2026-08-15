390f01147c8c1999025baa837c5b2aa44983cb78
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-15 14:56:24 +0700

    feat: add Tabel Perkalian 45 (C++)

---
+++ programs/multiplication_table_of_45.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_45.cpp
+// Tabel perkalian 45.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "45 x " << i << " = " << (45 * i) << "\n";
+    }
+    return 0;
+}
