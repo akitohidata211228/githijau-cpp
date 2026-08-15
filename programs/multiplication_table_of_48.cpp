e6b592e522c5a3e02cad9112bea06b42aefbf96c
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-15 20:18:16 +0700

    feat: add Tabel Perkalian 48 (C++)

---
+++ programs/multiplication_table_of_48.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_48.cpp
+// Tabel perkalian 48.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "48 x " << i << " = " << (48 * i) << "\n";
+    }
+    return 0;
+}
