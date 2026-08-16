2b6b892609b8300684a61fb7e1308b54c7119d26
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-16 12:30:29 +0700

    feat: add Tabel Perkalian 52 (C++)

---
+++ programs/multiplication_table_of_52.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_52.cpp
+// Tabel perkalian 52.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "52 x " << i << " = " << (52 * i) << "\n";
+    }
+    return 0;
+}
