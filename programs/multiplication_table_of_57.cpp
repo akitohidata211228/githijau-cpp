94f8df23800303ee6cc1b72b1d4875ec2c132068
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-16 22:24:04 +0700

    feat: add Tabel Perkalian 57 (C++)

---
+++ programs/multiplication_table_of_57.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_57.cpp
+// Tabel perkalian 57.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "57 x " << i << " = " << (57 * i) << "\n";
+    }
+    return 0;
+}
