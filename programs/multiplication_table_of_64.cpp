bedce11513f670f0fa51b567a017bd51a3836502
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-17 21:08:50 +0700

    feat: add Tabel Perkalian 64 (C++)

---
+++ programs/multiplication_table_of_64.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_64.cpp
+// Tabel perkalian 64.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "64 x " << i << " = " << (64 * i) << "\n";
+    }
+    return 0;
+}
