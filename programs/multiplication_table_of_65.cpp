af567684aaee12cbc2acc81f6e321f087fbe3329
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-17 22:53:50 +0700

    feat: add Tabel Perkalian 65 (C++)

---
+++ programs/multiplication_table_of_65.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_65.cpp
+// Tabel perkalian 65.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "65 x " << i << " = " << (65 * i) << "\n";
+    }
+    return 0;
+}
