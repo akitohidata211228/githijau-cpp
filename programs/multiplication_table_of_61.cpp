a1c097dae0e56d886ad04b05a5e33f9b273aa58b
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-17 16:11:11 +0700

    feat: add Tabel Perkalian 61 (C++)

---
+++ programs/multiplication_table_of_61.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_61.cpp
+// Tabel perkalian 61.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "61 x " << i << " = " << (61 * i) << "\n";
+    }
+    return 0;
+}
