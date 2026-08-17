8eb0cd1ee28a08042b3085d67454f7d3f9003a05
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-17 19:10:47 +0700

    feat: add Tabel Perkalian 63 (C++)

---
+++ programs/multiplication_table_of_63.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_63.cpp
+// Tabel perkalian 63.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "63 x " << i << " = " << (63 * i) << "\n";
+    }
+    return 0;
+}
