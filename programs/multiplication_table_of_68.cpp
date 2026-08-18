1ec0f9d063d6e33e480923cd89244a0b8faef6dd
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-18 14:15:28 +0700

    feat: add Tabel Perkalian 68 (C++)

---
+++ programs/multiplication_table_of_68.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_68.cpp
+// Tabel perkalian 68.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "68 x " << i << " = " << (68 * i) << "\n";
+    }
+    return 0;
+}
