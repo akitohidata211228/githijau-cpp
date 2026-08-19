20749f443ed004f9a250a180306041f08ff10175
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-19 18:26:44 +0700

    feat: add Tabel Perkalian 78 (C++)

---
+++ programs/multiplication_table_of_78.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_78.cpp
+// Tabel perkalian 78.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "78 x " << i << " = " << (78 * i) << "\n";
+    }
+    return 0;
+}
