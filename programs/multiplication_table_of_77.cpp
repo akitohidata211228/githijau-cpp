898180a8e62f78bb653c31fded1f8a87cc9408be
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-19 18:15:37 +0700

    feat: add Tabel Perkalian 77 (C++)

---
+++ programs/multiplication_table_of_77.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_77.cpp
+// Tabel perkalian 77.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "77 x " << i << " = " << (77 * i) << "\n";
+    }
+    return 0;
+}
