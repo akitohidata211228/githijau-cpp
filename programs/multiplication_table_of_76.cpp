bb823e2ad03a494f727ce13babcce4ae459d7000
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-19 14:57:56 +0700

    feat: add Tabel Perkalian 76 (C++)

---
+++ programs/multiplication_table_of_76.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_76.cpp
+// Tabel perkalian 76.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "76 x " << i << " = " << (76 * i) << "\n";
+    }
+    return 0;
+}
