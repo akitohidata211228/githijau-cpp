df63807fa7218f34978fc2051b2d7639fed91f3d
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-20 13:22:43 +0700

    feat: add Tabel Perkalian 83 (C++)

---
+++ programs/multiplication_table_of_83.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_83.cpp
+// Tabel perkalian 83.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "83 x " << i << " = " << (83 * i) << "\n";
+    }
+    return 0;
+}
