e48be04c6dd8c977b231a60bd20630c73310b2f0
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-20 22:44:59 +0700

    feat: add Tabel Perkalian 88 (C++)

---
+++ programs/multiplication_table_of_88.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_88.cpp
+// Tabel perkalian 88.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "88 x " << i << " = " << (88 * i) << "\n";
+    }
+    return 0;
+}
