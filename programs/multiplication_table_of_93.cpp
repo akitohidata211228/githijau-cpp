631ef136b812d7e82e3c78e21508df92dc013f75
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-21 17:29:59 +0700

    feat: add Tabel Perkalian 93 (C++)

---
+++ programs/multiplication_table_of_93.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_93.cpp
+// Tabel perkalian 93.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "93 x " << i << " = " << (93 * i) << "\n";
+    }
+    return 0;
+}
