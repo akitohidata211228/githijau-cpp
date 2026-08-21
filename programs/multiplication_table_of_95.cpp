7b17408379019cbb218571e22f333bb14c97a045
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-21 20:50:40 +0700

    feat: add Tabel Perkalian 95 (C++)

---
+++ programs/multiplication_table_of_95.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_95.cpp
+// Tabel perkalian 95.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "95 x " << i << " = " << (95 * i) << "\n";
+    }
+    return 0;
+}
