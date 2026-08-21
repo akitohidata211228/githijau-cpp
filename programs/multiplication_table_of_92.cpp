949d5b6f2f9d51b1d0d3f3d8e216f6aa0a0829d9
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-21 16:12:59 +0700

    feat: add Tabel Perkalian 92 (C++)

---
+++ programs/multiplication_table_of_92.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_92.cpp
+// Tabel perkalian 92.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "92 x " << i << " = " << (92 * i) << "\n";
+    }
+    return 0;
+}
