6672aa2978cb986c3abe45d9d18679470f903bbc
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-21 10:44:58 +0700

    feat: add Tabel Perkalian 89 (C++)

---
+++ programs/multiplication_table_of_89.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_89.cpp
+// Tabel perkalian 89.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "89 x " << i << " = " << (89 * i) << "\n";
+    }
+    return 0;
+}
