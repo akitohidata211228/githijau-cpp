d213a1ebc983d64017b4a36cb5f161d171960879
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-22 11:39:54 +0700

    feat: add Tabel Perkalian 98 (C++)

---
+++ programs/multiplication_table_of_98.cpp
@@ -0,0 +1,11 @@
+// multiplication_table_of_98.cpp
+// Tabel perkalian 98.
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "98 x " << i << " = " << (98 * i) << "\n";
+    }
+    return 0;
+}
